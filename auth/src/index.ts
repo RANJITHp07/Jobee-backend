00f1e6bfb15ea84fd676d9f18a9e2f65765612cc
Ranjth P <ranjithp27122003@gmail.com>
2024-02-12 10:35:38 +0530

    testing deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { connectDB } from "./infrastructure/config/connectdb"
 
 const app=createServer();
 
-//listening to the servers
+//listening to the servers and creating database
 connectDB().then(()=>{
     app?.listen(3000,()=>{
         console.log(`connected to the server`)
