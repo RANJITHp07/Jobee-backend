d3343725af4ebee592a0af22a56956fd8ed6b98a
Ranjth P <ranjithp27122003@gmail.com>
2024-02-19 12:38:24 +0530

    checking the env variables

--- photos/s3.ts
+++ photos/s3.ts
@@ -9,7 +9,7 @@ const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-
+console.log(bucketName,region,accessKeyId,secretAccessKey)
 
 let s3Client:any
 if(bucketName && region && accessKeyId && secretAccessKey){
